017d30072bd484cf33690c7fe119eb3ea853afe0
JoseParragaSG <85181855+JoseParragaSG@users.noreply.github.com>
2022-01-30 14:37:59 -0500

    Update game.js

--- game.js
+++ game.js
@@ -2,3 +2,20 @@ const initGame = () => {
     document.querySelector("#Menu").style.display = "none";
     document.querySelector("#Main").style.display = "block";
 };
+
+/*  Funcion aleatorioConProbabilidad
+      Parámetros de entrada:
+        min,max : Rango de números a elegir.
+        matrizNumeros : Array con los números con una probabilidad propia.
+        matrizProbabilidad : Array con la probabilidad (de 0 a 1) de cada
+          número de matrizNumeros.
+*/
+function aleatorioConProbabilidad (min,max,matrizNumeros,matrizProbabilidad){
+	for (let i=0; i<matrizProbabilidad.length; i++) {
+		if (Math.random()<matrizProbabilidad[i]) {
+			return matrizNumeros[i];
+        }
+    }
+	return Math.floor(Math.random()*(max-min+1)+min);
+}
+aleatorioConProbabilidad(1,4,[1,2,3,4],[.7,.5,.2,.01])
