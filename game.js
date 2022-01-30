02c1ab5da0563b45cc8d2cdbbe1ab90a9a12533d
JoseParragaSG <85181855+JoseParragaSG@users.noreply.github.com>
2022-01-30 15:29:53 -0500

    Update game.js

--- game.js
+++ game.js
@@ -12,10 +12,10 @@ const initGame = () => {
 */
 function aleatorioConProbabilidad (matrizNumeros,matrizProbabilidad){
 	for (let i=0; i<matrizProbabilidad.length; i++) {
-		if (Math.floor(Math.random()*71)<matrizProbabilidad[i]) {
+		if (Math.floor(Math.random()*142)<matrizProbabilidad[i]) {
 			return matrizNumeros[i];
         }
     }
 	//return Math.floor(Math.random()*(max-min+1)+min);
 }
-aleatorioConProbabilidad([4,3,2,1],[1,20,50,70])
+aleatorioConProbabilidad([1,2,3,4],[70,120,140,141])
