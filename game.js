7d5c1fa79d65854ae0c62e56d57e196ee96fb583
JoseParragaSG <85181855+JoseParragaSG@users.noreply.github.com>
2022-01-30 15:43:56 -0500

    Update game.js

--- game.js
+++ game.js
@@ -1,6 +1,8 @@
 const initGame = () => {
     document.querySelector("#Menu").style.display = "none";
     document.querySelector("#Main").style.display = "block";
+    console.log(aleatorioConProbabilidad([1,2,3,4],[70,120,140,141]))
+    timeraleatorio()
 };
 
 /*  Funcion aleatorioConProbabilidad
@@ -10,6 +12,12 @@ const initGame = () => {
         matrizProbabilidad : Array con la probabilidad (de 0 a 1) de cada
           número de matrizNumeros.
 */
+function timeraleatorio() {
+    setTimeout(function() {
+        console.log(aleatorioConProbabilidad([1,2,3,4],[70,120,140,141]))
+        timeraleatorio()
+    },5000)
+}
 function aleatorioConProbabilidad (matrizNumeros,matrizProbabilidad){
 	for (let i=0; i<matrizProbabilidad.length; i++) {
 		if (Math.floor(Math.random()*142)<matrizProbabilidad[i]) {
@@ -18,4 +26,4 @@ function aleatorioConProbabilidad (matrizNumeros,matrizProbabilidad){
     }
 	//return Math.floor(Math.random()*(max-min+1)+min);
 }
-aleatorioConProbabilidad([1,2,3,4],[70,120,140,141])
+//aleatorioConProbabilidad([1,2,3,4],[70,120,140,141])
