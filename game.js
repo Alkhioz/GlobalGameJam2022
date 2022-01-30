4c9018c7f4a25e73060dbffffac95b0c125d141e
JoseParragaSG <85181855+JoseParragaSG@users.noreply.github.com>
2022-01-30 15:50:47 -0500

    Update game.js

--- game.js
+++ game.js
@@ -1,3 +1,5 @@
+var dinero,eficiencia_energetica,tiempo
+
 const initGame = () => {
     document.querySelector("#Menu").style.display = "none";
     document.querySelector("#Main").style.display = "block";
@@ -5,19 +7,15 @@ const initGame = () => {
     timeraleatorio()
 };
 
-/*  Funcion aleatorioConProbabilidad
-      Parámetros de entrada:
-        min,max : Rango de números a elegir.
-        matrizNumeros : Array con los números con una probabilidad propia.
-        matrizProbabilidad : Array con la probabilidad (de 0 a 1) de cada
-          número de matrizNumeros.
-*/
+//FUNCION Q SE LLAMA A SI MISMA CADA 5 SEGUNDOS
 function timeraleatorio() {
     setTimeout(function() {
         console.log(aleatorioConProbabilidad([1,2,3,4],[70,120,140,141]))
         timeraleatorio()
     },5000)
 }
+
+//FUNCION Q RETORNA UNA OPCION CON PROBABILIDADES
 function aleatorioConProbabilidad (matrizNumeros,matrizProbabilidad){
 	for (let i=0; i<matrizProbabilidad.length; i++) {
 		if (Math.floor(Math.random()*142)<matrizProbabilidad[i]) {
